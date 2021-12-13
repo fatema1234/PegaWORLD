30904e539053384be4916272fdc3c021c1325879
fatema1234 <43412582+fatema1234@users.noreply.github.com>
2021-12-13 10:09:13 +0100

    Add files via upload

---
+++ PegaWORLD/script.js
@@ -0,0 +1,18 @@
+const toggleButton = document.getElementById('menuButton');
+const navList = document.getElementById('navbar-list');
+toggleButton.addEventListener('click', ()=> {
+  navList.classList.toggle('active');
+})
+/*const dropdownContent = document.querySelector(".dropdown-content");
+/*console.log(dropdown-content);*/
+/*dropdownContent.addEventListener("change", handleDropdownContent);
+function handleDropdownContent(e){
+console.log(e.target.value);
+}*/
+
+/*const dropDownButton = document.getElementById('dropdown-content');
+const dropDownList = document.getElementById('dropdown-links');
+dropDownList.addEventListener('click', () =>{
+dropDownButton.classList.toggle('active');
+})*/
+
